5340db3fb701551fcee6bffe1ce7cc56347ca7f9
poorneshpithani <eswar.chinni354@gmail.com>
2025-09-22 20:34:56 +0530

    adding frontend url into the backend server

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ console.log("Loading routes...");
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend address
+    origin: "https://college-management-app-eight.vercel.app", // frontend address
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
