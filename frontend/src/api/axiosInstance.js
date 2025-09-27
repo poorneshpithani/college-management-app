001b21f02a66b8f50e8167c03463125974b0ad26
Poornesh Pithani <102178667+poorneshpithani@users.noreply.github.com>
2025-09-27 21:32:59 +0530

    Update axiosInstance.js

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "https://college-management-app-ine5.onrender.com/api",
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+   baseURL: "https://college-management-app-ine5.onrender.com/api",
+  // baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
 // Attach token automatically
